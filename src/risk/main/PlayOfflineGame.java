e5a195b20bc6d38691d856d8e665ab6254aed351
cammalleri19Unipv <80333113+cammalleri19Unipv@users.noreply.github.com>
2021-08-18 16:47:28 +0200

    Update PlayOfflineGame.java

--- src/risk/main/PlayOfflineGame.java
+++ src/risk/main/PlayOfflineGame.java
@@ -13,12 +13,15 @@ public class PlayOfflineGame {
 	BufferedReader reader;
 	String line;
 	ArrayList<Player> Players;
-	
+	boolean attack;
+	boolean attackWin;
 	public PlayOfflineGame(ArrayList<Player> Players) {
 		fase=0;
 		playerTurn=0;
 		scelta=0;
 		this.Players=Players;
+		attack=true;
+		attackWin=false;
 		BufferedReader reader = new BufferedReader(
 	            new InputStreamReader(System.in));
 	}
@@ -67,17 +70,65 @@ public class PlayOfflineGame {
 			
 			//fase di attacco
 			case 2: {
-				while ()
+				while (attack!=false) {
+					System.out.println("vuoi attaccare? 1 si 0 no");
+					scelta= Integer.parseInt( reader.readLine());
+					if(scelta==1) {
+						System.out.println("i territori dal quale puoi attacccare sono :  scegli");//funzione che restituisce numero di territorio nome territorio e numero di carri
+						//nb si puo attaccare solo con 2 o piu carri 
+						scelta= Integer.parseInt( reader.readLine());
+						
+						//SELEZIONATO IL TERRITORIO SERVE UNA FUNZIONE CHE MOSTRA TUTTI I TERRITORI ATTACCABILI E LE LORO DIFESE 
+						
+						System.out.println("SELEZIONA CHI ATTACCHI E CON QUANTI CARRI :  scegli");
+						scelta= Integer.parseInt( reader.readLine());
+						
+						//gestire l'attacco
+						//se l'attacco viene vinto imposto attackWin a true (possono esserci piu attacchi quindi deve essere modificabile una volta sola)
+						//in caso di vittoria si possono spostare numero tot di carri - 1 nel nuovo territorio
+					}
+					else  attack=false;
+				}
+				fase++ ;
 				
 				
 			break;}
 			
 			//fase di spostamento
-			case 3:{
+			// e' solo una bozza , ovviamente mancano tutti i controlli di base ecc
+			case 3:{ //lista di tutti i territori che confinano con un tuo territorio che abbiano 2 o piu carri
 				
+				
+				System.out.println("SELEZIONA territorio da cui spostare ");
+				scelta= Integer.parseInt( reader.readLine());
+				System.out.println("SELEZIONA territorio di arrivo ");
+				scelta= Integer.parseInt( reader.readLine());
+				System.out.println("SELEZIONA numero di carri da spostare ");
+				scelta= Integer.parseInt( reader.readLine());
+			if(attackWin) {//DAI CARTA TERRITORIO
+				
+			}
+				
+			fase++;	
+			break;
+			}
 			
-			break;}
 			
+			// 	COME SI METTEVA IL DEFAULT ?? 
+			// manca l uscita dal ciclo in caso di vittoria
+			
+			case default{ 
+				fase=1;
+				playerTurn++;
+				attackWin=false;
+				attack=true;
+				if(playerTurn>= Players.size()) playerTurn=0;
+				
+				
+				break;
+			}
+			}
+	
 			
 			
 			}
@@ -86,4 +137,4 @@ public class PlayOfflineGame {
 		
 		
 	}
-}
+
