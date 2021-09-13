ac648c2a87467b814e866a259847bb2aed6bd81c
cammalleri19Unipv <80333113+cammalleri19Unipv@users.noreply.github.com>
2021-09-13 23:29:20 +0200

    aggiunta classe per test

---
+++ src/risk/test/RisikoGameTest.java
@@ -0,0 +1,45 @@
+package risk.test;
+import org.junit.Before;
+import org.junit.Test;
+
+import risk.model.Player;
+import risk.model.Territory;
+import risk.model.util.Color;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+class RisikoGameTest {
+	private Player Player1;
+	private	Player Player2;
+	private Territory  territory1;
+	private Territory  territory2;
+	@Before
+	public void setUp() {
+		Player1=new Player("pippo",Color.BLACK ,true);
+		Player1=new Player("pluto",Color.GREEN ,true);
+		territory1=new Territory("Italia",10,"europa");
+		territory2=new Territory("Germania",12,"europa");
+		territory1.addTanks(20);
+		territory1.addTanks(2);
+	
+	}
+	
+	
+	
+	@Test
+	public void testConquer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveTanks() {
+		
+		
+	}
+	
+}
