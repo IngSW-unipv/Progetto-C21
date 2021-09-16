829b3b2d908d66c484c3d5f140c0811780556678
AlessioPoggi99 <alessio.poggi01@universitadipavia.it>
2021-09-16 10:28:56 +0200

    predisposta grafica e metodo per il BLITZ

--- src/risk/controller/AttackController.java
+++ src/risk/controller/AttackController.java
@@ -23,25 +23,25 @@ import risk.model.RisikoGame;
 import risk.model.Territory;
 
 public class AttackController implements Initializable {
-	
+
 	DiceShaker attackDices;
 	DiceShaker defenderDices;
-	
+
 	@FXML
 	private AnchorPane rootPane;
-	
+
 	@FXML
 	private Label territoryAtkLabel, territoryDefLabel, attackerTanksLabel, defenderTanksLabel;
-	
+
 	@FXML
 	private Button attackButton, cancelButton, exitButton;
-	 
+
 	@FXML
 	private ToggleButton oneButton, twoButton,  threeButton;
-	
+
 	@FXML
 	private ImageView RedDice1,RedDice2,RedDice3,BlueDice1,BlueDice2,BlueDice3;
-	
+
 	private int atkTank, defTank;	// numero di tank usati per l'attacco e per la difesa
 	private int[] atkResults;		// risultati del lancio dei dadi dell'attaccante
 	private int[] defResults;
@@ -49,111 +49,111 @@ public class AttackController implements Initializable {
 	private Territory territory1= GameController.getInstance().getTerritory1();
 	private Territory territory2= GameController.getInstance().getTerritory2();
 	private SoundController soundController;
-	
-	
-	
-	
-	
+
+
+
+
+
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-    	attackButton.setDisable(true);
-    	territoryAtkLabel.setText(territory1.getName());
-    	territoryDefLabel.setText(territory2.getName());
-    	attackerTanksLabel.setText(String.valueOf(territory1.getTanks()));
-    	defenderTanksLabel.setText(String.valueOf(territory2.getTanks()));
-    	soundController = new SoundController();
-    	if(GameController.getInstance().getMusic()) soundController.battleMusic(); 
-    	setDefTank();
+		attackButton.setDisable(true);
+		territoryAtkLabel.setText(territory1.getName());
+		territoryDefLabel.setText(territory2.getName());
+		attackerTanksLabel.setText(String.valueOf(territory1.getTanks()));
+		defenderTanksLabel.setText(String.valueOf(territory2.getTanks()));
+		soundController = new SoundController();
+		if(GameController.getInstance().getMusic()) soundController.battleMusic(); 
+		setDefTank();
 		attackDices = new DiceShaker();
 		defenderDices = new DiceShaker();
 
 		atkResults = new int[3];
 		defResults = new int[3];
-    	
+
 		updateNumberButtons();
-		
+
 		// hide window on H hold pressed
 		rootPane.setOnKeyPressed((event) -> {
-		    if (event.getCode() == KeyCode.H) {
-		        //rootPane.getScene().getWindow().hide();
-		        rootPane.getScene().getWindow().setOpacity(0);
-		    }
+			if (event.getCode() == KeyCode.H) {
+				//rootPane.getScene().getWindow().hide();
+				rootPane.getScene().getWindow().setOpacity(0);
+			}
 		});
 		rootPane.setOnKeyReleased((event) -> {
-		    if (event.getCode() == KeyCode.H) {
-		        //rootPane.getScene().getWindow().show();
-		        rootPane.getScene().getWindow().setOpacity(1);
-		    }
+			if (event.getCode() == KeyCode.H) {
+				//rootPane.getScene().getWindow().show();
+				rootPane.getScene().getWindow().setOpacity(1);
+			}
 		});
 	}
-	
-    /**
-     * sets the number of defending tanks
-     */
-    private void setDefTank() {
-    	if(territory2.getTanks() > 2) {
-    		defTank = 3;
-    	}
-    	else
-    		defTank = territory2.getTanks();
-    }
-    
-    public void aiAttack(Territory t1,Territory t2) {
-    	
-    	int atNumber;
-    	int deNumber;
-    	
-    	if(t1.getTanks() > 3) {
-    		atNumber = 3;
-    	}
-    	else atNumber = t1.getTanks() -1;
-    	
-    	if(t2.getTanks() > 2) {
+
+	/**
+	 * sets the number of defending tanks
+	 */
+	private void setDefTank() {
+		if(territory2.getTanks() > 2) {
+			defTank = 3;
+		}
+		else
+			defTank = territory2.getTanks();
+	}
+
+	public void aiAttack(Territory t1,Territory t2) {
+
+		int atNumber;
+		int deNumber;
+
+		if(t1.getTanks() > 3) {
+			atNumber = 3;
+		}
+		else atNumber = t1.getTanks() -1;
+
+		if(t2.getTanks() > 2) {
 			deNumber = 3;
-    	} else {
-    		deNumber = t2.getTanks();
-    	}
-    	System.out.println("atnumber "+atNumber);
-    	System.out.println("denumber "+deNumber);
-    	System.out.println("t1 "+t1.getName());
-    	System.out.println("t2 "+t2.getName());
-    	
-    	DiceShaker attackDices2 = new DiceShaker();
+		} else {
+			deNumber = t2.getTanks();
+		}
+		System.out.println("atnumber "+atNumber);
+		System.out.println("denumber "+deNumber);
+		System.out.println("t1 "+t1.getName());
+		System.out.println("t2 "+t2.getName());
+
+		DiceShaker attackDices2 = new DiceShaker();
 		DiceShaker defenderDices2 = new DiceShaker();
 
 		int[] atkResults2 = new int[3];
 		int[] defResults2 = new int[3];
-		
+
 		atkResults2 = attackDices2.rollDices(atNumber);
 		defResults2 = defenderDices2.rollDices(deNumber);
-    	
-    	GameController.game.battle(atkResults2,defResults2, atNumber, deNumber,t1,t2);
-    	for(int i = 0 ; i < Math.max(atNumber, deNumber); i++) {
-    		System.out.println(""+atkResults2[i]);
-        	System.out.println(""+defResults2[i]);
-    	}
-    	
-    	GameController.getInstance().setPhaseTextArea(GameController.getInstance().getCurrentPlayer().getName()
+
+		GameController.game.battle(atkResults2,defResults2, atNumber, deNumber,t1,t2);
+		for(int i = 0 ; i < Math.max(atNumber, deNumber); i++) {
+			System.out.println(""+atkResults2[i]);
+			System.out.println(""+defResults2[i]);
+		}
+
+		GameController.getInstance().setPhaseTextArea(GameController.getInstance().getCurrentPlayer().getName()
 				+" attacked "+t2.getName()+" from "+t1.getName());
-    	
-    	
-    	if(t2.getOwner().equals(t1.getOwner())) {
-    		GameController.getInstance().setPhaseTextArea(GameController.getInstance().getCurrentPlayer().getName()
-    				+" conquered "+t2.getName());
+
+
+		if(t2.getOwner().equals(t1.getOwner())) {
+			GameController.getInstance().setPhaseTextArea(GameController.getInstance().getCurrentPlayer().getName()
+					+" conquered "+t2.getName());
 
 			GameController.game.giveCard();
-//			GameController.getInstance().setTerritory1(null);
-//	    	GameController.getInstance().setTerritory2(null);
+			//			GameController.getInstance().setTerritory1(null);
+			//	    	GameController.getInstance().setTerritory2(null);
 			GameController.getInstance().updateCardsNumber();
-			
-			
-    	}
 
-    }
 
-    
-    private void onClosing() {
+		}
+
+	}
+
+
+	private void onClosing() {
 		GameController.getInstance().setTerritory1(null);
 		GameController.getInstance().setTerritory2(null);
 		GameController.getInstance().clearAllTerritories();
@@ -162,23 +162,23 @@ public class AttackController implements Initializable {
 		GameController.getInstance().updateTerritoriesGraphic();
 		GameController.getInstance().playMusic();
 		if(GameController.getInstance().getMusic())soundController.stopMusic();
-    }
-    /**
-     * Manages the attack when the annulla button is pressed
-     * @param e is the event
-     * @throws IOException
-     */
+	}
+	/**
+	 * Manages the attack when the annulla button is pressed
+	 * @param e is the event
+	 * @throws IOException
+	 */
 	@FXML
-    public void cancelButtonPressed(ActionEvent e) throws IOException {
-		
+	public void cancelButtonPressed(ActionEvent e) throws IOException {
+
 		//GameController.getInstance().clearSelectedTerritory(GameController.getInstance().getAttacker());
 		//GameController.getInstance().clearSelectedTerritory(GameController.getInstance().getAttacker());
 		// deseleziono i territori attacker e defender
 		onClosing();
-    	Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
+		Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
 		window.close();
-    }
-	
+	}
+
 	@FXML	
 	private void attackButtonPressed(ActionEvent e) throws IOException {
 		ImageView[] attackerDiceImages = {RedDice1,RedDice2,RedDice3};
@@ -187,147 +187,150 @@ public class AttackController implements Initializable {
 		Image image1; 
 		InputStream defStream;
 		Image image2; 
-			
-			atkResults = attackDices.rollDices(atkTank);
-			defResults = defenderDices.rollDices(defTank);
-			GameController.game.battle(atkResults, defResults, atkTank, defTank, territory1,
-					territory2);
-			
-			GameController.getInstance().setPhaseTextArea(GameController.getInstance().getCurrentPlayer().getName()
-					+" attacked "+territory2.getName()+" from "+territory1.getName());
-			
-			if(atkTank == 2) {
-				atkStream = new FileInputStream("src/risk/view/images/dice/" +0+"_red.png");
-				image1 = new Image(atkStream);
-				attackerDiceImages[2].setImage(image1);	
-			}
-			
-			if(atkTank == 1) {
-				atkStream = new FileInputStream("src/risk/view/images/dice/" +0+"_red.png");
-				image1 = new Image(atkStream);
-				attackerDiceImages[1].setImage(image1);	
-				attackerDiceImages[2].setImage(image1);	
-			}
-			if(defTank == 2) {
-				defStream = new FileInputStream("src/risk/view/images/dice/" +0+"_blue.png");
-				image2 = new Image(defStream);
-				defenderDiceImages[2].setImage(image2);	
-			}
-			
-			if(defTank == 1) {
-				defStream = new FileInputStream("src/risk/view/images/dice/" +0+"_blue.png");
-				image2 = new Image(defStream);
-				defenderDiceImages[1].setImage(image2);	
-				defenderDiceImages[2].setImage(image2);	
-			}
-			for (int i = 0; i < Math.max(atkTank, defTank); i++) {
-				
-				System.out.println(atkResults[i] + "-------" + defResults[i]);
-				
-				
-				
-				atkStream = new FileInputStream("src/risk/view/images/dice/" + atkResults[i] +"_red.png");
-				image1 = new Image(atkStream);
-				attackerDiceImages[i].setImage(image1);	
-				defStream = new FileInputStream("src/risk/view/images/dice/" + defResults[i] +"_blue.png");
-				image2 = new Image(defStream);
-				defenderDiceImages[i].setImage(image2);	
 
-			}
-				
-	
-			attackerTanksLabel.setText(Integer.toString(territory1.getTanks()));
-			defenderTanksLabel.setText(Integer.toString(territory2.getTanks()));
-			updateNumberButtons();
-			setDefTank();
+		atkResults = attackDices.rollDices(atkTank);
+		defResults = defenderDices.rollDices(defTank);
+		GameController.game.battle(atkResults, defResults, atkTank, defTank, territory1,
+				territory2);
+
+		GameController.getInstance().setPhaseTextArea(GameController.getInstance().getCurrentPlayer().getName()
+				+" attacked "+territory2.getName()+" from "+territory1.getName());
+
+		if(atkTank == 2) {
+			atkStream = new FileInputStream("src/risk/view/images/dice/" +0+"_red.png");
+			image1 = new Image(atkStream);
+			attackerDiceImages[2].setImage(image1);	
+		}
+
+		if(atkTank == 1) {
+			atkStream = new FileInputStream("src/risk/view/images/dice/" +0+"_red.png");
+			image1 = new Image(atkStream);
+			attackerDiceImages[1].setImage(image1);	
+			attackerDiceImages[2].setImage(image1);	
+		}
+		if(defTank == 2) {
+			defStream = new FileInputStream("src/risk/view/images/dice/" +0+"_blue.png");
+			image2 = new Image(defStream);
+			defenderDiceImages[2].setImage(image2);	
+		}
+
+		if(defTank == 1) {
+			defStream = new FileInputStream("src/risk/view/images/dice/" +0+"_blue.png");
+			image2 = new Image(defStream);
+			defenderDiceImages[1].setImage(image2);	
+			defenderDiceImages[2].setImage(image2);	
+		}
+		for (int i = 0; i < Math.max(atkTank, defTank); i++) {
+
+			System.out.println(atkResults[i] + "-------" + defResults[i]);
 
 
-			if(GameController.getInstance().getTerritory2().getOwner().equals(GameController.getInstance().getTerritory1().getOwner())) {
-				defenderTanksLabel.setText("CONQUERED");
-				if(GameController.getInstance().getMusic())soundController.stopMusic();
-				if(GameController.getInstance().getMusic())soundController.conqueredSound();
-				GameController.game.giveCard();
-				GameController.getInstance().updateCardsNumber();
-				oneButton.setDisable(true);
-				twoButton.setDisable(true);
-				threeButton.setDisable(true);
-				attackButton.setDisable(true);
-				GameController.getInstance().updateTerritoriesGraphic();
-				
+
+			atkStream = new FileInputStream("src/risk/view/images/dice/" + atkResults[i] +"_red.png");
+			image1 = new Image(atkStream);
+			attackerDiceImages[i].setImage(image1);	
+			defStream = new FileInputStream("src/risk/view/images/dice/" + defResults[i] +"_blue.png");
+			image2 = new Image(defStream);
+			defenderDiceImages[i].setImage(image2);	
+
+		}
+
+
+		attackerTanksLabel.setText(Integer.toString(territory1.getTanks()));
+		defenderTanksLabel.setText(Integer.toString(territory2.getTanks()));
+		updateNumberButtons();
+		setDefTank();
+
+
+		if(GameController.getInstance().getTerritory2().getOwner().equals(GameController.getInstance().getTerritory1().getOwner())) {
+			defenderTanksLabel.setText("CONQUERED");
+			if(GameController.getInstance().getMusic())soundController.stopMusic();
+			if(GameController.getInstance().getMusic())soundController.conqueredSound();
+			GameController.game.giveCard();
+			GameController.getInstance().updateCardsNumber();
+			oneButton.setDisable(true);
+			twoButton.setDisable(true);
+			threeButton.setDisable(true);
+			attackButton.setDisable(true);
+			GameController.getInstance().updateTerritoriesGraphic();
+
+			try {
+				GameController.getInstance().windowLoader("/risk/view/fxml/InfosWindow.fxml", "Territory conquered", true, true);
+
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+
+
+			rootPane.getScene().getWindow().setOpacity(0);
+			if (GameController.getInstance().getTerritory1().getTanks() > 1) {
 				try {
-					GameController.getInstance().windowLoader("/risk/view/fxml/InfosWindow.fxml", "Territory conquered", true, true);
-					
+					GameController.getInstance().windowLoader("/risk/view/fxml/DisplacementScene.fxml", "Displacement", true, true);
 				} catch (IOException ex) {
 					ex.printStackTrace();
 				}
-
-
-				rootPane.getScene().getWindow().setOpacity(0);
-				if (GameController.getInstance().getTerritory1().getTanks() > 1) {
-					try {
-						GameController.getInstance().windowLoader("/risk/view/fxml/DisplacementScene.fxml", "Displacement", true, true);
-					} catch (IOException ex) {
-						ex.printStackTrace();
-					}
-				}
-				onClosing();
-		    	Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
-				window.close();
 			}
-
-			
+			onClosing();
+			Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
+			window.close();
 		}
-	
-	
-	
-	
-	
+
+
+	}
+
+	@FXML	
+	private void blitzButtonPressed(ActionEvent e) {
+		
+	}
+
+
+
 	@FXML
 	private void updateNumberButtons() {
-		
+
 		attackButton.setDisable(true);
 		oneButton.setSelected(false);
 		twoButton.setSelected(false);
 		threeButton.setSelected(false);
 		if(territory1.getTanks() == 1) {
 			oneButton.setDisable(true);
-    		twoButton.setDisable(true);
-    		threeButton.setDisable(true);
+			twoButton.setDisable(true);
+			threeButton.setDisable(true);
 		}
 		if(territory1.getTanks() == 2) {
-    		twoButton.setDisable(true);
-    		threeButton.setDisable(true);
-    	} else if(territory1.getTanks() == 3)
-    		threeButton.setDisable(true);
-    	
-    	oneButton.setOnAction(e -> {
-    		if(oneButton.isSelected()) {
-        		atkTank = 1;
-        		attackButton.setDisable(false);
-    		} else
-    			attackButton.setDisable(true);
-		});
-    	
-    	twoButton.setOnAction(e -> {
-    		if(twoButton.isSelected()) {
-        		atkTank = 2;
-        		attackButton.setDisable(false);
-    		} else
-    			attackButton.setDisable(true);
-		});
-    	
-    	threeButton.setOnAction(e -> {
-    		if(threeButton.isSelected()) {
-        		atkTank = 3;
-        		attackButton.setDisable(false);
-    		} else
-    			attackButton.setDisable(true);
-		});
-		
-	}
-	
-	
+			twoButton.setDisable(true);
+			threeButton.setDisable(true);
+		} else if(territory1.getTanks() == 3)
+			threeButton.setDisable(true);
 
- 
+		oneButton.setOnAction(e -> {
+			if(oneButton.isSelected()) {
+				atkTank = 1;
+				attackButton.setDisable(false);
+			} else
+				attackButton.setDisable(true);
+		});
+
+		twoButton.setOnAction(e -> {
+			if(twoButton.isSelected()) {
+				atkTank = 2;
+				attackButton.setDisable(false);
+			} else
+				attackButton.setDisable(true);
+		});
+
+		threeButton.setOnAction(e -> {
+			if(threeButton.isSelected()) {
+				atkTank = 3;
+				attackButton.setDisable(false);
+			} else
+				attackButton.setDisable(true);
+		});
+
+	}
+
+
+
+
 
 }
